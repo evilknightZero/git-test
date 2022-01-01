b39607b66158a01ed5aa378206042981b776120d
Hu <Hu@123.com>
2022-01-01 17:03:09 +0800

    second commit

--- src/main/java/com/workspace/git/gitTest.java
+++ src/main/java/com/workspace/git/gitTest.java
@@ -1,7 +1,11 @@
 package com.workspace.git;
 
+/**
+ * @author hu
+ */
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
